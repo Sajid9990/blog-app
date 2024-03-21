fa58ccfa2b2706ac02c56a700ce222aee69af5f0
Sajid Saifi <projectwork433@gmail.com>
2024-03-21 14:19:28 +0530

    code correction

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { Fragment, useEffect } from 'react';
+import { BrowserRouter, Route, Router, Routes } from 'react-router-dom';
+function App() {
+
+  return (
+    <Fragment>
+      {/* <BrowserRouter> */}
+        {/* <Routes> */}
+          {/* <Route path="/" exact element={<Game />} />
+          <Route path="/other" exact element={<OtherComp />} /> */}
+        {/* </Routes> */}
+      {/* </BrowserRouter> */}
+      Hello world
+    </Fragment>
+  );
+}
+
+export default App;
