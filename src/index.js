c13f23886e95a4d6eef2576ae9f1bd281ef8bfea
Sajid Saifi <projectwork433@gmail.com>
2024-03-22 12:14:20 +0530

    code correction

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
 
-import "assets/css/nucleo-icons.css";
-import "assets/scss/blk-design-system-react.scss";
-import "assets/demo/demo.css";
+import "./assets/css/nucleo-icons.css";
+import "./assets/scss/blk-design-system-react.scss";
+import "./assets/demo/demo.css";
 
-import Index from "views/Index.js";
-import LandingPage from "views/examples/LandingPage.js";
-import RegisterPage from "views/examples/RegisterPage.js";
-import ProfilePage from "views/examples/ProfilePage.js";
+import Index from "./views/Index.js";
+import LandingPage from "./views/examples/LandingPage.js";
+import RegisterPage from "./views/examples/RegisterPage.js";
+import ProfilePage from "./views/examples/ProfilePage.js";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
--- src/views/examples/ProfilePage.js
+++ src/views/examples/ProfilePage.js
@@ -44,22 +44,22 @@ import {
 } from "reactstrap";
 
 // core components
-import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
-import Footer from "components/Footer/Footer.js";
+import ExamplesNavbar from "./../../components/Navbars/ExamplesNavbar.js";
+import Footer from "./../../components/Footer/Footer.js";
 
 const carouselItems = [
   {
-    src: require("assets/img/denys.jpg"),
+    src: require("./../../assets/img/denys.jpg"),
     altText: "Slide 1",
     caption: "Big City Life, United States",
   },
   {
-    src: require("assets/img/fabien-bazanegue.jpg"),
+    src: require("./../../assets/img/fabien-bazanegue.jpg"),
     altText: "Slide 2",
     caption: "Somewhere Beyond, United States",
   },
   {
-    src: require("assets/img/mark-finn.jpg"),
+    src: require("./../../assets/img/mark-finn.jpg"),
     altText: "Slide 3",
     caption: "Stocks, United States",
   },
@@ -97,12 +97,12 @@ export default function ProfilePage() {
           <img
             alt="..."
             className="dots"
-            src={require("assets/img/dots.png")}
+            src={require("./../../assets/img/dots.png")}
           />
           <img
             alt="..."
             className="path"
-            src={require("assets/img/path4.png")}
+            src={require("./../../assets/img/path4.png")}
           />
           <Container className="align-items-center">
             <Row>
@@ -160,7 +160,7 @@ export default function ProfilePage() {
                     <img
                       alt="..."
                       className="img-center img-fluid rounded-circle"
-                      src={require("assets/img/mike.jpg")}
+                      src={require("./../../assets/img/mike.jpg")}
                     />
                     <h4 className="title">Transactions</h4>
                   </CardHeader>
