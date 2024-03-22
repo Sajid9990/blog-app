04534ff0669120d769a7ff9452057bbecc7dbb85
Sajid Saifi <projectwork433@gmail.com>
2024-03-22 17:01:34 +0530

    code correction

---
+++ src/component/homePage.js
@@ -0,0 +1,12 @@
+import { Fragment, useEffect } from 'react';
+
+function HomePage() {
+
+    return (
+        <Fragment>
+            <h1>Home Page</h1>
+        </Fragment>
+    );
+}
+
+export default HomePage;
