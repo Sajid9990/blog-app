4ad900f59e022f7906c900fea8a6a2bee067eab6
sajid saifi <433sajid@gmail.com>
2024-03-23 11:20:21 +0530

    f

--- src/component/homePage.js
+++ src/component/homePage.js
@@ -5,10 +5,14 @@ import { Link } from 'react-router-dom';
 
 function HomePage() {
     const [latestArtciles, setLatestArticles] = useState([]);
+    const [pageSize, setPageSize] = useState(24);
+    const [pageNumber, setPageNumber] = useState(Math.ceil(Math.random() * 10)); // rendom page number
+
 
     const getLatestArticles = async () => {
+
         // debugger
-        const response = await axios.get("https://api.thefactmagic.com/articles/latest/1/1/Vivo?page=5&size=24");
+        const response = await axios.get(`https://api.thefactmagic.com/articles/latest/1/1/Vivo?page=${pageNumber}&size=${pageSize}`);
 
         if (response.status == 200) {
             setLatestArticles(response.data.content);
@@ -19,40 +23,48 @@ function HomePage() {
     useEffect(() => {
         getLatestArticles();
     }, [])
- 
+
 
     console.log(latestArtciles);
 
 
     return (
         <Fragment>
-          <Container>
-          <Row className='p-5'>
-            {
-                latestArtciles.length > 0 ?
-                    latestArtciles.map((article, index) => {
-                        return (
-                            <Col md={3} key={index} className='mb-4'>
-                                <Card style={{ width: '18rem' }}>
-                                    <Card.Img variant="top" src={article.icon_image} />
-                                    <Card.Body>
-                                        <Card.Title>{article.title}</Card.Title>
-                                        <Card.Text>
-                                            {article.description}
-                                        </Card.Text>
-                                        <Button className='btn btn-dark'>
-                                            <Link to={`/read-blog/mazingbytes${article.id}/${article.slug}`} className='text-decoration-none text-light'>Read Article ...</Link>
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        );
-                    })
-                    : null
-            }
-            </Row>
-          </Container>
-            
+            <Container>
+
+                <Row className='p-3'>
+                    <Row className='mb-3'>
+                        <Col>
+                            <h4 style={{borderBottom:"2px solid black"}} className='p-2'>
+                                Trending Articles <span class="badge badge-success rounded bg-dark">{pageSize}</span>
+                            </h4>
+                        </Col>
+                    </Row>
+                    {
+                        latestArtciles.length > 0 ?
+                            latestArtciles.map((article, index) => {
+                                return (
+                                    <Col md={3} key={index} className='mb-4'>
+                                        <Card style={{ width: '18rem' }}>
+                                            <Card.Img variant="top" src={article.icon_image} />
+                                            <Card.Body>
+                                                <Card.Title>{article.title}</Card.Title>
+                                                <Card.Text>
+                                                    {article.description}
+                                                </Card.Text>
+                                                <Button className='btn btn-dark'>
+                                                    <Link to={`/read-blog/mazingbytes${article.id}/${article.slug}`} className='text-decoration-none text-light'>Read Article ...</Link>
+                                                </Button>
+                                            </Card.Body>
+                                        </Card>
+                                    </Col>
+                                );
+                            })
+                            : null
+                    }
+                </Row>
+            </Container>
+
         </Fragment>
     );
 }
